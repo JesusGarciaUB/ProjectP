053a8bc93e7843c970d0766bb84dd180b14e35b8
JesusGarciaUB <jesus.garcia@enti.cat>
2023-03-21 16:41:54 +0100

    a
    a

--- Assets/scripts/GlobalVariables.cs
+++ Assets/scripts/GlobalVariables.cs
@@ -26,6 +26,7 @@ public class GlobalVariables : MonoBehaviour
 
     private void Awake()
     {
+        canScroll = true;
         currentLevel = 0;
         score = 0;
         if (Instance == null)
